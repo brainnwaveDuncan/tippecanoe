6eb040cf00fee2ab1a3ec9d35816673cb352e01b
Duncan Paterson <duncan@brainnwave.com>
2018-08-03 15:46:50 +0100

    try not incrementing cluster count by one

--- tile.cpp
+++ tile.cpp
@@ -1976,14 +1976,14 @@ long long write_tile(FILE *geoms, std::atomic<long long> *geompos_in, char *meta
 
 				p.full_keys.push_back("point_count");
 				sv2.type = mvt_double;
-				sv2.s = std::to_string(p.clustered + 1);
+				sv2.s = std::to_string(p.clustered);
 				p.full_values.push_back(sv2);
 
 				add_tilestats(layername, z, layermaps, tiling_seg, layer_unmaps, "point_count", sv2);
 
 				p.full_keys.push_back("sqrt_point_count");
 				sv3.type = mvt_double;
-				sv3.s = std::to_string(round(100 * sqrt(p.clustered + 1)) / 100.0);
+				sv3.s = std::to_string(round(100 * sqrt(p.clustered)) / 100.0);
 				p.full_values.push_back(sv3);
 
 				add_tilestats(layername, z, layermaps, tiling_seg, layer_unmaps, "sqrt_point_count", sv3);
